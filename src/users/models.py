d658a39a6eef424ca834ea4fa8b15c0424be7e26
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-11-17 22:19:50 +0800

    removed unused class

--- src/users/models.py
+++ src/users/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 from src.database import Base
 from sqlalchemy.orm import relationship
