886a95e9c77d6e2cd1d797d7a0818d89dda2d13f
John Kristan Torremocha <138425887+johnkristanf@users.noreply.github.com>
2025-11-19 08:07:04 +0800

    clean router

--- src/expenses/router.py
+++ src/expenses/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio.session import AsyncSession
 
+from src.expenses.dependencies import get_expense_service
 from src.expenses.service import ExpenseService
 from src.database import Database
 from src.expenses.schemas import ExpenseChatIn
@@ -8,14 +8,10 @@ from src.expenses.schemas import ExpenseChatIn
 expense_router = APIRouter()
 
 
-@expense_router.post("/create", status_code=200)
+@expense_router.post("/create", status_code=201)
 async def create_expense(
     payload: ExpenseChatIn,
-    session: AsyncSession = Depends(Database.get_async_session),
-    # DEPENDENCY INJECT THE FETCHING OF THE CURRENT USER (JWT-BASED)
+    expense_service: ExpenseService = Depends(get_expense_service)
 ):
-    current_user = {"id": 1, "name": "John Kristan"}
-
-    expense_service = ExpenseService(session, current_user["id"])
     result = await expense_service.agent_expense_insert(payload.message)
-    return {"result": result}
+    return result
