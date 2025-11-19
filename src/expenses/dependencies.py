af13b192975aeccd60bbd427e11013f8e329837b
John Kristan Torremocha <138425887+johnkristanf@users.noreply.github.com>
2025-11-19 08:06:50 +0800

    inject db session and user dependencies

--- src/expenses/dependencies.py
+++ src/expenses/dependencies.py
@@ -4,6 +4,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import Database
 from src.expenses.service import ExpenseService
 
-
-async def get_expense_service() -> ExpenseService:
-    return ExpenseService
+# Dependency that accepts arguments from other dependencies
+async def get_expense_service(
+    session: AsyncSession = Depends(Database.get_async_session),
+    current_user: dict = Depends(lambda: {"id": 1, "name": "John Kristan"}),  # Example user
+) -> ExpenseService:
+    return ExpenseService(session, current_user["id"])
