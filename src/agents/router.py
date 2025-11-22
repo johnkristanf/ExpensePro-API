dfae9bd4ee9d1ebb7957afa0aee401a3a232debb
John Kristan Torremocha <138425887+johnkristanf@users.noreply.github.com>
2025-11-22 08:04:02 +0800

    agent response

--- src/agents/router.py
+++ src/agents/router.py
@@ -1,3 +1,6 @@
+import json
+import logging
+from collections import defaultdict
 from fastapi import APIRouter, Depends
 from fastapi.responses import StreamingResponse
 
@@ -5,9 +8,13 @@ from src.agents.dependencies import get_agent
 from src.agents.schemas import AgentChatIn
 from src.agents.agent import Agent
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
 agent_router = APIRouter()
-
-
 
 @agent_router.post("/chat", status_code=201)
 async def agent_chat(
@@ -21,11 +28,40 @@ async def agent_chat(
             {"messages": [{"role": "user", "content": payload.message}]},
             version="v2",
         ):
+            print(f"EVENT DATA: {event}")
+            # -----------------------------
+            # LOG TOOL START
+            # -----------------------------
+            if event["event"] == "on_tool_start":
+                tool = event["name"]
+                # print(f"EVENT START TOOL DATA : {event['data']}")
+                # logger.info("🔧 TOOL START\nTool: %s\nArgs: %s", tool, json.dumps(tool_result, indent=2, default=str))
+
+            # -----------------------------
+            # LOG TOOL END
+            # -----------------------------
+            if event["event"] == "on_tool_end":
+                tool = event["name"]
+                # print(f"EVENT END TOOL DATA : {event['data']}")
+                # logger.info("✅ TOOL END\nTool: %s\nOutput: %s", tool, json.dumps(tool_result, indent=2, default=str))
+
+            # -----------------------------
+            # LOG TOOL ERROR
+            # -----------------------------
+            if event["event"] == "on_tool_error":
+                tool = event["name"]
+                error = event["data"]["error"]
+                print("\n❌ TOOL ERROR")
+                print(f"Tool: {tool}")
+                print(f"Error: {error}\n")
+
+            # -----------------------------
+            # STREAM MODEL TOKENS
+            # -----------------------------
             if event["event"] == "on_chat_model_stream":
-                print(f"DATA CHUNK: {event['data']['chunk']}")
-                token = event["data"]["chunk"].content
-                if token:
-                    yield token
+                chunk = event["data"]["chunk"].content
+                if chunk:
+                    yield chunk
 
         yield "[END]"
 
